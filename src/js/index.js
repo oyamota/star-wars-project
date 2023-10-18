25d87f5b86c11a2decd0059f483c7578aaa9291d
Alessandra Mota <alemotac@gmail.com>
2023-10-17 22:38:11 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+        botao.classList.add("selecionado");
+
+        const personagensSelecionados = document.querySelectorAll(".personagem.selecionado");
+        personagensSelecionados.forEach(personagem => {
+            personagem.classList.remove("selecionado");
+        });
+
+        personagens[indice].classList.add("selecionado");
+    });
+});
